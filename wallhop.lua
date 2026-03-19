e236a9489b41232bb7f6c4009f4bcf70d1f61657
netzwii-dev <netzwii@gmail.com>
2026-03-19 04:41:53 -0300

    Update wallhop.lua

--- wallhop.lua
+++ wallhop.lua
@@ -1,7 +1,7 @@
 --[[
     Auto Wall Hop Script (Video Recreation Version)
     - Flick 45° direita
-    - Tempo: 0.065
+    - Tempo: 0.038
     - Botão travado (sem drag)
     - Posição fixa abaixo do chat
 ]]
@@ -68,7 +68,7 @@ local function performVideoFlick()
     local startCFrame = Camera.CFrame
     Camera.CFrame = startCFrame * CFrame.Angles(0, math.rad(45), 0)
 
-    task.wait(0.065)
+    task.wait(0.038)
 
     Camera.CFrame = startCFrame
 
